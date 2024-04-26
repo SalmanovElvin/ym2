9b3de1cfcd0694122542260de619d43eba42a346
SalmanovElvin <elvin.salmanov@uniontechnologiesinc.com>
2024-04-26 11:25:20 +0400

    notifications page

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -1,24 +1,73 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Button, FlatList, Image } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList, Image, TouchableOpacity } from "react-native";
 
 import Svg, { G, Circle, Path, Defs, ClipPath, Rect } from "react-native-svg";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useMutation, useQuery } from "@apollo/client";
+import { MARK_NOTIFICATION } from "../../../graph/mutations/notifications";
 
 export const Notification = ({ navigation, notification }) => {
-        const originalDate = new Date(notification?.createdOn);
-        const formattedDate = `${originalDate.getFullYear()}-${(originalDate.getMonth() + 1).toString().padStart(2, '0')}-${originalDate.getDate().toString().padStart(2, '0')} ${originalDate.getHours().toString().padStart(2, '0')}:${originalDate.getMinutes().toString().padStart(2, '0')}:${originalDate.getSeconds().toString().padStart(2, '0')}`;
+    const originalDate = new Date(notification?.createdOn);
+    const formattedDate = `${originalDate.getFullYear()}-${(originalDate.getMonth() + 1).toString().padStart(2, '0')}-${originalDate.getDate().toString().padStart(2, '0')} ${originalDate.getHours().toString().padStart(2, '0')}:${originalDate.getMinutes().toString().padStart(2, '0')}:${originalDate.getSeconds().toString().padStart(2, '0')}`;
 
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const userVal = await AsyncStorage.getItem("@USER"); // Replace 'key' with your actual key
+
+                if (userVal !== null && JSON.parse(userVal).username !== undefined) {
+                    setUserData(JSON.parse(userVal));
+                } else {
+                    console.log("No user data found");
+                }
+            } catch (error) {
+                console.error("Error retrieving data:", error);
+            }
+        };
+        getData();
+        if (notification.read == false) {
+            setIsRead(false);
+        }
+    }, []);
+
+    const [isRead, setIsRead] = useState(true);
+
+    //Mutation to mark notification as read
+    const [
+        markNotificationAsRead,
+        { loading: markLoading, error: markError, data: markData },
+    ] = useMutation(MARK_NOTIFICATION, {
+        refetchQueries: ['notifications'],
+    });
+
+    const readNotification = () => {
+        markNotificationAsRead({
+            variables: {
+                unionID: userData.unionID,
+                notificationID: notification.id,
+                userID: userData.id,
+            },
+            onCompleted: () => {
+                setIsRead(true);
+                // console.log('read');
+            },
+            onError: (err) => {
+                console.log(err);
+            }
+        });
+    }
     return (
-        <View style={styles.wrapper}>
-            {notification.read == false ?
+        <TouchableOpacity onLongPress={readNotification} activeOpacity={0.6} style={styles.wrapper}>
+            {isRead == false ?
                 <View style={styles.nonRead}></View>
                 :
                 <></>}
             <Text style={{ color: '#242529', fontWeight: '400', fontSize: 16 }}>{notification.message}</Text>
             <Text style={{ color: '#848587', fontSize: 14, fontWeight: '400', marginTop: 5 }}>{formattedDate}</Text>
-        </View>
+        </TouchableOpacity>
     );
 };
 
