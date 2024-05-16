25e23b6f3bf7aa9a512ac07eaf3a3ae267bb25c2
SalmanovElvin <elvin.salmanov@uniontechnologiesinc.com>
2024-05-16 18:56:14 +0400

    adding false to header.js 61 line

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -58,6 +58,7 @@ export const Header = () => {
             userID: userData?.id,
         },
         onCompleted: () => {
+            setIsNotifications(false);
             for (let i = 0; i < data?.notifications?.length; i++) {
                 if (data.notifications[i].read == false) {
                     setIsNotifications(true);
