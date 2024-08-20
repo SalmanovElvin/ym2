7e4a125a8882d1d2e8ff8431bf447fe4b853ae47
SalmanovElvin <elvin.salmanov@uniontechnologiesinc.com>
2024-08-20 15:43:35 +0400

    memorium

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -561,7 +561,7 @@ export const SettingsScreen = ({ navigation, route }) => {
                     marginLeft: 10,
                   }}
                 >
-                  Version - 1.3.8
+                  Version - 1.3.9
                 </Text>
               </View>
             </View>
